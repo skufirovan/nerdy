bf7577cf36f3341ab1a084c696dac1a00223b4c0
Khaspalaev Anush <khaspalaevan05@gmail.com>
2025-07-19 03:36:35 +0300

    refctor: simplify initUserMeta middleware

--- src/middlewares/initUserMeta/index.ts
+++ src/middlewares/initUserMeta/index.ts
@@ -1,30 +1,11 @@
+import { MiddlewareFn } from "telegraf";
 import { MyContext } from "@bot/features/scenes";
-import { UserController } from "@controller";
-import userActionsLogger from "@infrastructure/logger/userActionsLogger";
 
-export const initUserMeta = async (
-  ctx: MyContext,
-  next: () => Promise<void>
-) => {
+export const initUserMeta: MiddlewareFn<MyContext> = async (ctx, next) => {
   const accountId = ctx.from?.id ? BigInt(ctx.from.id) : null;
   const username = ctx.from?.username ?? null;
 
   if (!accountId) return ctx.reply("⚠️ Не удалось определить Telegram ID");
-
-  try {
-    const user = await UserController.findByAccountId(accountId);
-
-    if (user && user.username !== username) {
-      await UserController.updateUserInfo(accountId, { username });
-    }
-  } catch (error) {
-    const err = error instanceof Error ? error.message : String(error);
-    userActionsLogger("error", "initUserMeta", `${err}`, {
-      accountId,
-      username,
-    });
-    await ctx.reply("🚫 Произошла ошибка. Попробуйте позже.");
-  }
 
   ctx.user = { accountId, username };
   return next();
