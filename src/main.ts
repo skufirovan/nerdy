7ca160abf99acc9967ca4147207f7e036ad3c133
Khaspalaev Anush <khaspalaevan05@gmail.com>
2025-07-25 02:43:33 +0300

    chore: handle uncaught exceptions and unhandled rejections

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { bot } from "@bot/index";
 
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception:", error);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+  process.exit(1);
+});
+
 (async () => {
   try {
     await bot.launch();
